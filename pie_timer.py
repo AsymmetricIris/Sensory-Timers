97928ca08a26d2c286bc9606ecdb12a988e156a6
David <david.ingleby@tutanota.com>
2023-08-09 18:29:40 +0100

    pie_timer.py now produces a visual timer with timespan defined by the minutes variable. Timer does the job for me. || TODO - implement a QML version using Julia

--- pie_timer.py
+++ pie_timer.py
@@ -1,22 +1,30 @@
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 from matplotlib.animation import FuncAnimation
 import csv
 
 fig, ax = plt.subplots()
 
-time_remaining = 40
-time_elapsed = 5
+minutes = 45
+
+time_remaining = minutes*60
+time_elapsed = 0
 size=[time_remaining,time_elapsed]
 labels=["remaining", "elapsed"]
-ax.pie(size, labels=labels)
+colors=['g','w']
+ax.pie(size, labels=labels, colors=colors)
 
 def myupdate(i):
+    global time_remaining
+    global time_elapsed
+    global colors
+
     if (time_remaining > 0):
         ax.clear()
         time_remaining -= 1
         time_elapsed += 1
         size=[time_remaining,time_elapsed]
-        ax.pie(size, labels=labels)
+        ax.pie(size, labels=labels, colors=colors)
     
 myanimation=FuncAnimation(fig, myupdate, interval=1000)
 plt.show()
